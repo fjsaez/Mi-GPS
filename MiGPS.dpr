1312fc4aa6f3a238619d9797b6e390ed04c53b81
Francisco Sáez <fjsaez@gmail.com>
2023-05-08 22:18:13 -0400

    Sustituido código obsoleto

--- MiGPS.dpr
+++ MiGPS.dpr
@@ -18,7 +18,8 @@ uses
 
 begin
   Application.Initialize;
-  SharedActivity.getWindow.addFlags(TJWindowManager_LayoutParams.JavaClass.FLAG_KEEP_SCREEN_ON);
+  TAndroidHelper.Activity.getWindow.addFlags(
+    TJWindowManager_LayoutParams.JavaClass.FLAG_KEEP_SCREEN_ON);
   Application.FormFactor.Orientations := [TFormOrientation.Portrait];
   Application.CreateForm(TFPrinc, FPrinc);
   Application.Run;
